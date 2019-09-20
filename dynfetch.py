32bd92b4ad97d7f746b3a6893c2596600c332dc0
Jan Kapellen <jan.kapellen@statravel.com>
2019-09-20 13:09:20 +0200

    fixing a calculation error, outputting a csv file, variable name clarification

--- dynfetch.py
+++ dynfetch.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import configparser
+import csv
 from datetime import date
 from datetime import timedelta
-from csv import reader
 from dyn.tm.session import DynectSession
 from dyn.tm.zones import get_all_zones
 from dyn.tm.errors import DynectGetError
@@ -17,34 +17,44 @@ session = DynectSession(config['dyn']['accountname'],
 
 zonelist = get_all_zones()
 
-all_recs = 0
-all_reqs = 0
+sum_records = 0
+sum_requests = 0
 
 start = date.today() - timedelta(days=30)
+
+zones = dict()
 
 def csvsum(csvvar):
     """
     cumulates the second column in the csv and returns it
     """
     sum = 0
-    intread = reader(csvvar.splitlines())
+    intread = csv.reader(csvvar.splitlines())
     next(intread)  # to skip the first line
     for row in intread:
-        effq = int(row[1]) * 3600
-        sum += effq
+        sum += int(row[1])
     return sum
 
 
 for zone in zonelist:
     try:
         allrecs = zone.get_all_records()
-        all_recs += len(allrecs)
+        sum_records += len(allrecs)
         qpscsv = zone.get_qps(start_ts=start)
         effqs = csvsum(qpscsv['csv'])
-        all_reqs += effqs
+        sum_requests += effqs
         print("Zone \"{0}\" has {1} records and gets about {2}k queries per month".format(zone.fqdn, len(allrecs), effqs/1000))
+        zones[zone.fqdn]['requests'] = effqs
+        zones[zone.fqdn]['records'] = len(allrecs)
     except DynectGetError:
         print("Zone \"{0}\" does not want to list records (secondary zone?)".format(zone.fqdn))
 
 
-print("{0} records in {1} zones, approximately {2}k queries per month".format(all_recs, len(zonelist), all_reqs/1000))
+print("{0} records in {1} zones, approximately {2}k queries per month".format(sum_records, len(zonelist), sum_requests/1000))
+
+with open('dyn-fetch.csv', 'w', newline='') as outcsv:
+    csvw = csv.writer(outcsv, dialect='excel')
+    csvw.writerow(['Zone FQDN', 'Number of Records', 'Number of Requests(Month)'])
+    for zone in zones.items():
+        csvw.writerow([zone, zones[zone]['records'], zones[zone]['requests']])
+    csvw.writerow(['TOTALS', sum_records, sum_requests])
